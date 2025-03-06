f05c1a471a2209e1b6e4f503755ab471f580b383
Bruno Duarte <bruno.duarte314@gmail.com>
2025-03-06 18:44:16 -0300

    feat: add BrowserRouter at App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import { ThemeProvider } from "styled-components";
+import { BrowserRouter } from "react-router";
+
 import { defaultTheme } from "@themes";
 import { GlobalStyle } from "@styles";
+
+import { Router } from "./Router";
 
 export const App = () => {
   return (
     <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+
       <GlobalStyle />
     </ThemeProvider>
   );
