147c95c1400ab0e12eb31372e840066eb1935420
Bruno Duarte <bruno.duarte314@gmail.com>
2025-03-06 18:39:48 -0300

    refactor: add paths aliases

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import { ThemeProvider } from "styled-components";
-import { defaultTheme } from "./themes/default";
+import { defaultTheme } from "@themes";
+import { GlobalStyle } from "@styles";
 
 export const App = () => {
-  return <ThemeProvider theme={defaultTheme}></ThemeProvider>;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+    </ThemeProvider>
+  );
 };
